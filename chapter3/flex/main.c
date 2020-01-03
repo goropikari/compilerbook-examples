7cd236171e85de52eb11374a5b2d9dbfe41eb8f3
goropikari <goropikari56@gmail.com>
2020-01-03 21:43:28 +0900

    input from stdin or args

--- chapter3/flex/main.c
+++ chapter3/flex/main.c
@@ -5,17 +5,22 @@ extern FILE *yyin;
 extern int yylex();
 extern char *yytext;
 
-int main()
+int main(int argc, char const* argv[])
 {
-	yyin = fopen("program.c","r");
-	if(!yyin) {
-		printf("could not open program.c!\n");
-		return 1;
-	}
+    if (argc < 2) {
+        yyin = stdin;
+    } else {
+        yyin = fopen(argv[1], "r");
+    }
 
-	while(1) {
-		token_t t = yylex();
-		if(t==TOKEN_EOF) break;
-		printf("token: %d  text: %s\n",t,yytext);
-	}
+    if(!yyin) {
+        printf("could not open program.c!\n");
+        return 1;
+    }
+
+    while(1) {
+        token_t t = yylex();
+        if(t==TOKEN_EOF) break;
+        printf("token: %d  text: %s\n",t,yytext);
+    }
 }
